0c9e66c56d697899b2eecb80f6ca0ba65634c580
John Stegman <greenleafservices.gls@gmail.com>
2017-07-11 14:31:32 +0000

    07-11-17-A

--- app/assets/javascripts/customers.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click', '.custRow', function(){
-    alert ("clicked")
-    var custId = $(this).children('td').html(); //gets emp id
-    $.get('/customers.js?id=' + custId).success(function(){
-      // $('#issuCard').css({'visibility':'visible'});// show card
-    });
-});
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [:edit, :update, :destroy]
 
   # GET /customers
-  # GET /customers.json
+  
   def index
     @customers = Customer.by_last_name
     #*************** Process for generating a csv file ****************
@@ -15,14 +15,11 @@ class CustomersController < ApplicationController
   end
 
   # GET /customers/1
-  # GET /customers/1.json
+  
   def show
     # @customer = Customer.find(params[:id])
     @customer = Customer.includes(:equipment, :csas).find(params[:id])
-    respond_to do |format|
-      format.html { render action: "show" }
-      # format.json { render json: @user.errors, status: :unprocessable_entity }
-    end
+    
   end
 
   # GET /customers/new
@@ -35,7 +32,7 @@ class CustomersController < ApplicationController
   end
 
   # POST /customers
-  # POST /customers.json
+  
   def create
     
     @customer = Customer.new(customer_params)
@@ -43,45 +40,40 @@ class CustomersController < ApplicationController
     respond_to do |format|
       if @customer.save
         format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
-        format.json { render :show, status: :created, location: @customer }
       else
         format.html { render :new }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
+  
   def update
     respond_to do |format|
       if @customer.update(customer_params)
         format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @customer }
       else
         format.html { render :edit }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /customers/1
-  # DELETE /customers/1.json
+  
   def destroy
     @customer.destroy
     respond_to do |format|
       format.html { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_customer
       @customer = Customer.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    
     def customer_params
       
       params.require(:customer).permit(:cust_id, :first_name, :last_name, :address1, :address2, :city, :st, :zip, :home_phone, :work_phone, :cell_phone, :email, :auto_serv, :notes)
